5c7043a1e464f74d632612c84cf6910bb2063f76
SlightlyMadMax <makcandreev2001@mail.ru>
2023-10-10 21:52:48 +0300

    add get_analytic_solution function for 1d 2f solution

--- src/tests/num_vs_analytic/analytic_solution_1d_2f.py
+++ src/tests/num_vs_analytic/analytic_solution_1d_2f.py
@@ -33,22 +33,40 @@ t_0 = (s_0 / gamma)**2
 print(f"STARTING TIME (S = {s_0} m): {int(t_0/3600)} hours")
 
 
-def get_t_ice(y: float):
-    return g * (erf(s_0 / (2.0 * a_ice * t_0 ** 0.5)) - erf(y / (2.0 * a_ice * t_0 ** 0.5))) / \
-        erf(s_0 / (2.0 * a_ice * t_0 ** 0.5))
+def get_ice_temp(y: float, _s_0: float, _t_0: float):
+    return g * (erf(_s_0 / (2.0 * a_ice * _t_0 ** 0.5)) - erf(y / (2.0 * a_ice * _t_0 ** 0.5))) / \
+        erf(_s_0 / (2.0 * a_ice * _t_0 ** 0.5))
 
 
-def get_t_water(y: float):
-    return u_0 * (erf(y / (2.0 * a_water * t_0 ** 0.5)) - erf(s_0 / (2.0 * a_water * t_0 ** 0.5))) / \
-        (1.0 - erf(s_0 / (2.0 * a_water * t_0 ** 0.5)))
+def get_water_temp(y: float, _s_0: float, _t_0: float):
+    return u_0 * (erf(y / (2.0 * a_water * _t_0 ** 0.5)) - erf(_s_0 / (2.0 * a_water * _t_0 ** 0.5))) / \
+        (1.0 - erf(_s_0 / (2.0 * a_water * _t_0 ** 0.5)))
 
 
 analytic_T = np.empty((N_Y, N_X))
 
-
 for j in range(N_Y):
-    analytic_T[j, :] = get_t_ice(j * dy) if j * dy <= s_0 else get_t_water(j * dy)
+    analytic_T[j, :] = get_ice_temp(j * dy, s_0, t_0) if j * dy <= s_0 else get_water_temp(j * dy, s_0, t_0)
+
 
 np.savez_compressed("./data/analytic_1d_2f_at_0_3", T=analytic_T)
 
 plot_temperature(analytic_T, t_0, 0, True)
+
+
+def get_analytic_solution(_s_0: float):
+    """
+    Функция для нахождения аналитического решения модельной одномерной двухфазной задачи при заданных параметрах.
+    :param _s_0: Начальное положение границы.
+    :return: Распределение температуры для модельной задачи в момент времени, соответсвующий положению свободной
+    границы s_0.
+    """
+    result = np.empty((N_Y, N_X))
+
+    _gamma = fsolve(trans_eq, 0.0002)[0]
+    _t_0 = (_s_0 / _gamma) ** 2
+
+    for j in range(N_Y):
+        result[j, :] = get_ice_temp(j * dy, _s_0, _t_0) if j * dy <= _s_0 else get_water_temp(j * dy, _s_0, _t_0)
+
+    return result
