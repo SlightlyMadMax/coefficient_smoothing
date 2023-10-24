f4d150439694c1c037d13f9c9d936f3146db03ee
SlightlyMadMax <makcandreev2001@mail.ru>
2023-10-24 17:41:59 +0300

    update get_phase_trans_boundary, so it now returns a set of (x, y) points

--- src/boundary.py
+++ src/boundary.py
@@ -25,15 +25,24 @@ def get_phase_trans_boundary(T, n_x: int = N_X):
     Функция для определения границы фазового перехода по изотерме T(x, y) = T_0
     :param T: Двумерный массив температур на текущем временном слое.
     :param n_x: Число узлов по оси X.
-    :return: Вектор координат границы фазового перехода на текущем временном слое.
+    :return: Кортеж векторов (X, Y) с точками границы фазового перехода на текущем временном слое.
     """
-    F = np.empty(n_x)
+    X = []
+    Y = []
 
     for j in range(1, N_Y - 1):
-        for i in range(n_x):
+        for i in range(1, n_x - 1):
             if (T[j, i] - T_0) * (T[j + 1, i] - T_0) < 0.0:
                 if abs(T[j, i] - T_0) <= abs(T[j + 1, i] - T_0):
-                    F[i] = j * dy
+                    Y.append(j * dy)
                 else:
-                    F[i] = (j + 1) * dy
-    return F
+                    Y.append((j + 1) * dy)
+                X.append(i*dx)
+            elif (T[j, i] - T_0) * (T[j, i + 1] - T_0) < 0.0:
+                if abs(T[j, i] - T_0) <= abs(T[j, i + 1] - T_0):
+                    X.append(i * dx)
+                else:
+                    X.append((i + 1) * dx)
+                Y.append(j*dy)
+
+    return X, Y
