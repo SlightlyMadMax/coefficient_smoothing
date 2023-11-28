501392c0aabe40955cb828d85dc24dcae3d327df
SlightlyMadMax <makcandreev2001@mail.ru>
2023-11-28 11:54:58 +0300

    consider width parameter when plotting temperature

--- src/plotting.py
+++ src/plotting.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from src.parameters import N_Y, N_X, HEIGHT, dt, dx, dy, T_WATER_MAX, T_ICE_MIN
+from src.parameters import N_Y, N_X, HEIGHT, WIDTH, dt, dx, dy, T_WATER_MAX, T_ICE_MIN
 from src.boundary import get_phase_trans_boundary
 
 
 def plot_temperature(T, time: float, graph_id: int, plot_boundary: bool = False, show_graph: bool = True):
-    x = np.linspace(0, 1.0, N_X)
+    x = np.linspace(0, WIDTH, N_X)
     y = np.linspace(0, HEIGHT, N_Y)
 
     X, Y = np.meshgrid(x, y)
@@ -16,15 +16,15 @@ def plot_temperature(T, time: float, graph_id: int, plot_boundary: bool = False,
 
     # plt.plot(X, Y, marker=".", markersize=0.5, color='k', linestyle='none')  # сетка
 
-    plt.contourf(X, Y, T, 50, cmap="viridis")
+    plt.contourf(X, Y, T, 50, cmap="viridis", vmin=T_ICE_MIN, vmax=T_WATER_MAX)
 
     if plot_boundary:
         X_b, Y_b = get_phase_trans_boundary(T)
         plt.scatter(X_b, Y_b, s=1, linewidths=0.1, color='r', label='Граница ф.п.')
         ax.legend()
 
-    # plt.ylim((4.5, 10.05))
-    # plt.xlim((0.3, 0.7))
+    # plt.ylim((4.0, 5.0))
+    # plt.xlim((4.0, 5.0))
 
     plt.clim(T_ICE_MIN, T_WATER_MAX)
 
