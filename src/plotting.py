0101c217e650839135401ffe4080bfe62dbb3e26
SlightlyMadMax <makcandreev2001@mail.ru>
2023-12-05 21:02:54 +0300

    fix incorrect colorbar

--- src/plotting.py
+++ src/plotting.py
@@ -17,6 +17,8 @@ def plot_temperature(T, time: float, graph_id: int, plot_boundary: bool = False,
     # plt.plot(X, Y, marker=".", markersize=0.5, color='k', linestyle='none')  # сетка
 
     plt.contourf(X, Y, T, 50, cmap="viridis", vmin=T_ICE_MIN, vmax=T_WATER_MAX)
+    plt.colorbar()
+    plt.clim(T_ICE_MIN, T_WATER_MAX)
 
     if plot_boundary:
         X_b, Y_b = get_phase_trans_boundary(T)
@@ -26,9 +28,6 @@ def plot_temperature(T, time: float, graph_id: int, plot_boundary: bool = False,
     # plt.ylim((4.0, 5.0))
     # plt.xlim((4.0, 5.0))
 
-    plt.clim(T_ICE_MIN, T_WATER_MAX)
-
-    plt.colorbar()
     ax.set_xlabel("x, м")
     ax.set_ylabel("y, м")
     ax.set_title(f"t = {int(time/3600)} ч.\n dx = {round(dx, 3)} m, dy = {round(dy, 3)} m, dt = {round(dt, 2)} с")
