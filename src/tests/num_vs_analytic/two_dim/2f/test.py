02c9c1ef6d6553dbf542c7a60ace72996375f815
SlightlyMadMax <makcandreev2001@mail.ru>
2024-03-03 14:53:47 +0300

    update 2d2f test

--- src/tests/num_vs_analytic/two_dim/2f/test.py
+++ src/tests/num_vs_analytic/two_dim/2f/test.py
@@ -8,14 +8,16 @@ from src.geometry import DomainGeometry
 from src.solver import solve
 from src.plotting import plot_temperature, animate
 from src.temperature import init_temperature_2f_test
-from src.boundary import get_phase_trans_boundary
+from src.parameters import K_ICE, K_WATER, RHO_ICE, L
 
+T_WATER = 7.0
+T_ICE = -3.0
+end_time = 60.0 * 60.0 * 24.0 * 350.0
+n_t = 24*350 + 1
+T_0 = 0.0
 
-T_WATER = 5.0
-T_ICE = -5.0
-end_time = 60.0 * 60.0 * 24.0 * 300.0
-n_t = 24*300 + 1
-
+b_lim = (K_WATER * L / RHO_ICE) * T_WATER / ((K_WATER * L / RHO_ICE) * T_WATER + (K_ICE * L / RHO_ICE)*abs(T_ICE))
+print(f"Theoretical boundary final position: {1.0 - b_lim}")
 
 geom = DomainGeometry(
     width=1.0,
@@ -59,22 +61,25 @@ for i in range(1, n_t+1):
               fixed_delta=False
               )
     if i % 24 == 0:
-        T_full.append(T)
-        times.append(t)
+        # T_full.append(T)
+        # times.append(t)
         print(f"ВРЕМЯ МОДЕЛИРОВАНИЯ: {i} ч, ВРЕМЯ ВЫПОЛНЕНИЯ: {time.process_time() - start_time}")
 
+for j in range(1, geom.n_y - 1):
+    if (T[j, 250] - T_0) * (T[j + 1, 250] - T_0) < 0.0:
+        y_0 = abs((T[j, 250] * (j + 1) * geom.dy - T[j + 1, 250] * j * geom.dy) / (T[j, 250] - T[j + 1, 250]))
+        print(f"Calculated final location of the boundary: {y_0}")
+        print(f"Absolute error: {abs(y_0 - 1 + b_lim)}, relative: {round(abs(y_0 - 1 + b_lim) * 100/ b_lim, 2)}%")
+        break
 
-print("СОЗДАНИЕ АНИМАЦИИ...")
-animate(
-    T_full=T_full,
-    geom=geom,
-    times=times,
-    t_step=60*60*24,
-    directory="./results/",
-    filename="test_animation",
-    min_temp=T_ICE,
-    max_temp=T_WATER
-)
-
-X, Y = get_phase_trans_boundary(T, geom)
-print(Y[250][250])
+# print("СОЗДАНИЕ АНИМАЦИИ...")
+# animate(
+#     T_full=T_full,
+#     geom=geom,
+#     times=times,
+#     t_step=60*60*24,
+#     directory="./results/",
+#     filename="test_animation",
+#     min_temp=T_ICE,
+#     max_temp=T_WATER
+# )
