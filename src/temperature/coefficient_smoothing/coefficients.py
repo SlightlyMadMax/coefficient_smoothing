68d088d6928870a22d0ba3a5502cc5428ee4492b
Maxim <lmaokeklol@gmail.com>
2024-12-20 00:28:41 +0300

    rename "_delta" to "delta" in coefficients.py

--- src/temperature/coefficient_smoothing/coefficients.py
+++ src/temperature/coefficient_smoothing/coefficients.py
@@ -3,17 +3,17 @@ import numba
 
 
 @numba.jit(nopython=True)
-def delta_function(u_j_i: float, u_0: float, _delta: float) -> float:
+def delta_function(u_j_i: float, u_0: float, delta: float) -> float:
     """
     Smoothed approximation of the delta function, centered at u_0.
 
     :param u_j_i: The temperature value at the (j, i) node of the coordinate grid.
     :param u_0: The point where the delta function is centered.
-    :param _delta: The smoothing parameter.
+    :param delta: The smoothing parameter.
     :return: The value of the smoothed delta function at the point u_j_i.
     """
-    return math.exp(-(u_j_i - u_0) * (u_j_i - u_0) / (2.0 * _delta * _delta)) / (
-        (2.0 * math.pi) ** 0.5 * _delta
+    return math.exp(-(u_j_i - u_0) * (u_j_i - u_0) / (2.0 * delta * delta)) / (
+        (2.0 * math.pi) ** 0.5 * delta
     )
 
 
@@ -25,7 +25,7 @@ def c_smoothed(
     c_solid: float,
     c_liquid: float,
     l_solid: float,
-    _delta: float,
+    delta: float,
 ) -> float:
     """
     Smoothed effective volumetric heat capacity.
@@ -36,20 +36,20 @@ def c_smoothed(
     :param c_solid: The volumetric heat capacity of the solid phase.
     :param c_liquid: The volumetric heat capacity of the liquid phase.
     :param l_solid: The volumetric latent heat of fusion of the solid phase.
-    :param _delta: The smoothing parameter.
+    :param delta: The smoothing parameter.
     :return: The value of the smoothed effective volumetric heat capacity at the temperature u_j_i.
     """
     u_pt = u_pt - u_ref
 
-    if _delta <= 0:
+    if delta <= 0:
         return c_solid if u_j_i < u_pt else c_liquid
 
     return (
         c_solid
         + (c_liquid - c_solid)
-        * (1.0 + math.erf((u_j_i - u_pt) / (2**0.5 * _delta)))
+        * (1.0 + math.erf((u_j_i - u_pt) / (2**0.5 * delta)))
         * 0.5
-        + l_solid * delta_function(u_j_i=u_j_i, u_0=u_pt, _delta=_delta)
+        + l_solid * delta_function(u_j_i=u_j_i, u_0=u_pt, delta=delta)
     )
 
 
@@ -60,7 +60,7 @@ def k_smoothed(
     u_ref: float,
     k_solid: float,
     k_liquid: float,
-    _delta: float,
+    delta: float,
 ) -> float:
     """
     Smoothed heat conductivity coefficient.
@@ -70,33 +70,33 @@ def k_smoothed(
     :param u_ref: The reference temperature.
     :param k_solid: The heat conductivity of the solid phase.
     :param k_liquid: The heat conductivity of the liquid phase.
-    :param _delta: The smoothing parameter.
+    :param delta: The smoothing parameter.
     :return: The value of the smoothed heat conductivity coefficient at the temperature u_j_i.
     """
     u_pt = u_pt - u_ref
 
-    if _delta <= 0.0:
+    if delta <= 0.0:
         return k_solid if u_j_i < u_pt else k_liquid
 
     return (
         k_solid
         + (k_liquid - k_solid)
-        * (1.0 + math.erf((u_j_i - u_pt) / (2**0.5 * _delta)))
+        * (1.0 + math.erf((u_j_i - u_pt) / (2**0.5 * delta)))
         * 0.5
     )
 
 
 @numba.jit(nopython=True)
-def delta_parabolic(u_j_i: float, u_pt: float, _delta: float) -> float:
-    if abs(u_j_i - u_pt) <= _delta:
-        return 0.75 * (1.0 - u_j_i * u_j_i / (_delta * _delta)) / _delta
+def delta_parabolic(u_j_i: float, u_pt: float, delta: float) -> float:
+    if abs(u_j_i - u_pt) <= delta:
+        return 0.75 * (1.0 - u_j_i * u_j_i / (delta * delta)) / delta
     return 0.0
 
 
 @numba.jit(nopython=True)
-def delta_const(u_j_i: float, u_pt: float, _delta: float) -> float:
-    if abs(u_j_i - u_pt) <= _delta:
-        return 0.5 / _delta
+def delta_const(u_j_i: float, u_pt: float, delta: float) -> float:
+    if abs(u_j_i - u_pt) <= delta:
+        return 0.5 / delta
     return 0.0
 
 
@@ -108,13 +108,13 @@ def c_simple(
     c_solid: float,
     c_liquid: float,
     l_solid: float,
-    _delta: float,
+    delta: float,
 ) -> float:
     u_pt = u_pt - u_ref
 
-    if abs(u_j_i - u_pt) <= _delta:
-        return 0.5 * (c_liquid + c_solid) + l_solid * delta_const(u_j_i, _delta)
-    elif u_j_i < u_pt - _delta:
+    if abs(u_j_i - u_pt) <= delta:
+        return 0.5 * (c_liquid + c_solid) + l_solid * delta_const(u_j_i, delta)
+    elif u_j_i < u_pt - delta:
         return c_solid
     else:
         return c_liquid
@@ -127,13 +127,13 @@ def k_simple(
     u_ref: float,
     k_solid: float,
     k_liquid: float,
-    _delta: float,
+    delta: float,
 ) -> float:
     u_pt = u_pt - u_ref
 
-    if abs(u_j_i - u_pt) <= _delta:
-        return k_liquid + 0.5 * (k_liquid - k_solid) * (u_j_i - u_pt - _delta) / _delta
-    elif u_j_i < u_pt - _delta:
+    if abs(u_j_i - u_pt) <= delta:
+        return k_liquid + 0.5 * (k_liquid - k_solid) * (u_j_i - u_pt - delta) / delta
+    elif u_j_i < u_pt - delta:
         return k_solid
     else:
         return k_liquid
