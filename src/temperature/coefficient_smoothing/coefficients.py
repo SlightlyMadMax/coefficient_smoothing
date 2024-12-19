a0d91130359e31d40638eb521b4bd920eab38176
Maxim <lmaokeklol@gmail.com>
2024-12-20 00:26:29 +0300

    docstrings in English for smoothed coefficients

--- src/temperature/coefficient_smoothing/coefficients.py
+++ src/temperature/coefficient_smoothing/coefficients.py
@@ -3,14 +3,16 @@ import numba
 
 
 @numba.jit(nopython=True)
-def delta_function(u_j_i: float, u_pt: float, _delta: float) -> float:
+def delta_function(u_j_i: float, u_0: float, _delta: float) -> float:
     """
-    Сглаженная аппроксимация дельта-функции, сосредоточенная в T_0.
-    :param u_j_i: Значение температуры в (i, j) узле координатной сетки.
-    :param _delta: Параметр сглаживания.
-    :return: Значение сглаженной дельта-функции в точке u_j_i.
+    Smoothed approximation of the delta function, centered at u_0.
+
+    :param u_j_i: The temperature value at the (j, i) node of the coordinate grid.
+    :param u_0: The point where the delta function is centered.
+    :param _delta: The smoothing parameter.
+    :return: The value of the smoothed delta function at the point u_j_i.
     """
-    return math.exp(-(u_j_i - u_pt) * (u_j_i - u_pt) / (2.0 * _delta * _delta)) / (
+    return math.exp(-(u_j_i - u_0) * (u_j_i - u_0) / (2.0 * _delta * _delta)) / (
         (2.0 * math.pi) ** 0.5 * _delta
     )
 
@@ -26,10 +28,16 @@ def c_smoothed(
     _delta: float,
 ) -> float:
     """
-    Сглаженная эффективная объемная теплоемкость.
-    :param u_j_i: Значение температуры в (i, j) узле координатной сетки.
-    :param _delta: Параметр сглаживания.
-    :return: Значение сглаженной эффективной объемной теплоемкости при температуре u_j_i.
+    Smoothed effective volumetric heat capacity.
+
+    :param u_j_i: The temperature value at the (j, i) node of the coordinate grid.
+    :param u_pt: The phase transition temperature.
+    :param u_ref: The reference temperature.
+    :param c_solid: The volumetric heat capacity of the solid phase.
+    :param c_liquid: The volumetric heat capacity of the liquid phase.
+    :param l_solid: The volumetric latent heat of fusion of the solid phase.
+    :param _delta: The smoothing parameter.
+    :return: The value of the smoothed effective volumetric heat capacity at the temperature u_j_i.
     """
     u_pt = u_pt - u_ref
 
@@ -41,7 +49,7 @@ def c_smoothed(
         + (c_liquid - c_solid)
         * (1.0 + math.erf((u_j_i - u_pt) / (2**0.5 * _delta)))
         * 0.5
-        + l_solid * delta_function(u_j_i=u_j_i, u_pt=u_pt, _delta=_delta)
+        + l_solid * delta_function(u_j_i=u_j_i, u_0=u_pt, _delta=_delta)
     )
 
 
@@ -55,10 +63,15 @@ def k_smoothed(
     _delta: float,
 ) -> float:
     """
-    Сглаженный коэффициент теплопроводности.
-    :param u_j_i: Значение температуры в (i, j) узле координатной сетки.
-    :param _delta: Параметр сглаживания.
-    :return: Значение сглаженного коэффициента теплопроводности при температуре T_j_i.
+    Smoothed heat conductivity coefficient.
+
+    :param u_j_i: The temperature value at the (j, i) node of the coordinate grid.
+    :param u_pt: The phase transition temperature.
+    :param u_ref: The reference temperature.
+    :param k_solid: The heat conductivity of the solid phase.
+    :param k_liquid: The heat conductivity of the liquid phase.
+    :param _delta: The smoothing parameter.
+    :return: The value of the smoothed heat conductivity coefficient at the temperature u_j_i.
     """
     u_pt = u_pt - u_ref
 
